92c28e7c309e601bf0f7e755681251c3d49bfb52
Soumyadeb-2023-33 <soumyadebnandy23@gmail.com>
2024-03-18 22:05:22 +0530

    my code

--- Codechef/practice/PCL01_problems_START01.c
+++ Codechef/practice/PCL01_problems_START01.c
@@ -5,11 +5,7 @@ int main() {
     int N;
 
     printf("Enter a number: ");
-
-    // reads the user input stores in 'N'
     scanf("%d", &N);
-
-    // print the 'N'
     printf("The number you entered is: %d", N);
 
     return 0;
