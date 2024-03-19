072ee8383d1e8bd31e0e44928bca9e04e5e427a7
Soumyadeb-2023-33 <soumyadebnandy23@gmail.com>
2024-03-19 15:01:15 +0530

    my code

---
+++ College Lab/prb3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+int x,y;
+printf("intput x:");
+scanf("%d",&x);
+printf("intput y:");
+scanf("%d",&y);
+
+if(x>=y){
+    printf("bigger x");
+}
+else{
+    printf("bigger y");
+}
+
+    return 0;
+}
