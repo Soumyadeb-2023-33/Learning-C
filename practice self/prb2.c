848d9e0ef9aff0ec365ee92cab6d4a6c80a15172
Soumyadeb-2023-33 <soumyadebnandy23@gmail.com>
2024-03-19 11:02:40 +0530

    my code

--- practice self/prb2.c
+++ practice self/prb2.c
@@ -1,10 +1,26 @@
-//volume of a sphere
+#include <stdio.h>
 
-#include<stdio.h>
-int main(){
-    float r=5;
-    float pi=3.14;
-    float V=4*pi*r*r*r/3;
-    printf("The Volume is : %f",V);
+int replace_zeros_with_ones(int num) {
+    int result = 0;
+    int power = 1;
+    while (num != 0) {
+        int digit = num % 10;
+        if (digit != 0) {
+            result += digit * power;
+        } else {
+            result += 1 * power;
+        }
+        power *= 10;
+        num /= 10;
+    }
+    return result;
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    int replaced = replace_zeros_with_ones(num);
+    printf("The number with all 0's replaced with 1's is %d\n", replaced);
     return 0;
 }
